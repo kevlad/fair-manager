f35b6d58f58c815e7124ac9d4d72c9e6a11eccea
kevlad <64752511+kevlad@users.noreply.github.com>
2025-05-11 17:32:14 +0300

    Create typeorm.config.service.ts

---
+++ srcdatabase/typeorm.config.service.ts
@@ -0,0 +1,32 @@
+---
+**File: `iam-service/src/database/typeorm.config.service.ts`** (If using TypeORM)
+---
+```typescript
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const dbConfig = this.configService.get('database');
+    return {
+      type: dbConfig.type,
+      host: dbConfig.host,
+      port: dbConfig.port,
+      username: dbConfig.username,
+      password: dbConfig.password,
+      database: dbConfig.database,
+      entities: dbConfig.entities,
+      synchronize: dbConfig.synchronize,
+      migrationsRun: dbConfig.migrationsRun,
+      migrations: dbConfig.migrations,
+      logging: dbConfig.logging,
+      charset: dbConfig.charset,
+      extra: dbConfig.extra,
+      autoLoadEntities: true, // Recommended with NestJS
+    };
+  }
+}
