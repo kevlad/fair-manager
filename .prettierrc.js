e511f9943042f755ad07e8b291b3d4c4d7d66d1e
kevlad <64752511+kevlad@users.noreply.github.com>
2025-05-11 17:17:15 +0300

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+---
+**File: `iam-service/.prettierrc.js`**
+---
+```javascript
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 100, // Adjust as per team preference
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
